44d83e8ffab44ed3d23bf3cfabf71c511c0656c5
daffa tabiano <daffatabianoo@gmail.com>
2025-07-18 22:18:11 +0700

    feat(utils): all utility features

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+export const listNavbar = ["ABOUT US" , "PORTFOLIO", "LOCATION"];
+
+export const homes = [
+  {
+    name: 'BASE 1',
+    location: 'Pandeyan, Yogyakarta',
+    bedrooms: '2 Bedrooms',
+    price: 'Pre-launch: Prices TBA',
+    description: 'Our flagship model encompasses all the essentials. It’s compact yet spacious, efficient yet adaptable.',
+    image: '/images/carousel/carousel-1.jpg',
+  },
+  {
+    name: 'BASE 2',
+    location: 'Ngaglik, Yogyakarta',
+    bedrooms: '3 Bedrooms',
+    price: 'Pre-launch: Prices TBA',
+    description: 'Our flagship model encompasses all the essentials. It’s compact yet spacious, efficient yet adaptable.',
+    image: '/images/carousel/carousel-2.jpg',
+  },
+  {
+    name: 'BASE 3',
+    location: 'Ngemplak, Yogyakarta',
+    bedrooms: '4 Bedrooms',
+    price: 'Pre-launch: Prices TBA',
+    description: 'Our flagship model encompasses all the essentials. It’s compact yet spacious, efficient yet adaptable.',
+    image: '/images/carousel/carousel-3.jpg',
+  },
+  {
+    name: 'BASE 4',
+    location: 'Tegal sari, Yogyakarta',
+    bedrooms: '2 Bedrooms',
+    price: 'Pre-launch: Prices TBA',
+    description: 'Our flagship model encompasses all the essentials. It’s compact yet spacious, efficient yet adaptable.',
+    image: '/images/carousel/carousel-4.jpg',
+  },
+  {
+    name: 'BASE 5',
+    location: 'Seturan, Yogyakarta',
+    bedrooms: '3 Bedrooms',
+    price: 'Pre-launch: Prices TBA',
+    description: 'Our flagship model encompasses all the essentials. It’s compact yet spacious, efficient yet adaptable.',
+    image: '/images/carousel/carousel-5.jpg',
+  },
+];
